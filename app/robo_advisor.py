7f2e9f9465e7377c6cb1f791f80f43c4404b0b33
NikoRestifo <ndr45@georgetown.edu>
2021-03-03 23:55:36 -0500

    Write example data to csv

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,7 +1,11 @@
 # this is the "app/robo_advisor.py" file
 
-import requests
+import csv
 import json
+import os
+
+import requests
+
 
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
@@ -41,6 +45,24 @@ for date in dates:
 recent_high = max(high_prices)
 recent_low = min(low_prices)
 
+#
+# INFO OUTPUTS
+#
+
+
+#csv_file_path = "data/prices.csv" # a relative filepath
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -55,5 +77,8 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print("WRITING DATA TO CSV...")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
